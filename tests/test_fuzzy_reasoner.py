db24b8b98295a7c3854afb5e8b483b3ede12537e
David Chanin <chanindav@gmail.com>
2022-04-25 19:59:10 +0100

    fixing flaky version test

--- tests/test_fuzzy_reasoner.py
+++ tests/test_fuzzy_reasoner.py
@@ -1,5 +1,21 @@
-from fuzzy_reasoner import __version__
+from fuzzy_reasoner import (
+    SLDProver,
+    Atom,
+    Constant,
+    Predicate,
+    Rule,
+    Variable,
+    cosine_similarity,
+    symbol_compare,
+)
 
 
-def test_version() -> None:
-    assert __version__ == "0.1.0"
+def test_imports() -> None:
+    assert SLDProver is not None
+    assert Atom is not None
+    assert Constant is not None
+    assert Predicate is not None
+    assert Rule is not None
+    assert Variable is not None
+    assert cosine_similarity is not None
+    assert symbol_compare is not None
